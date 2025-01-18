f0c33029e30267441d81711c601ad8e7749326b1
maheshy6 <maheshy675@gmail.com>
2025-01-18 19:25:25 +0530

    #f

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 import "dotenv/config"
 import mongoose from "mongoose";
 import userRoute from "./router/user.router.js";
+import cors from "cors"
 
 const app = express()
 app.use(express.json())
 app.use("/api/users",userRoute)
+app.use(cors())
 const port = process.env.PORT || 2020
 app.listen(port, async()=>{
     await mongoose.connect(process.env.MONGODB_CLOUD_URL)
--- model/user.model.js
+++ model/user.model.js
@@ -2,7 +2,9 @@ import {Schema,model} from "mongoose";
 const userSchema=new Schema({
       username:{type:String, required:true},
       email:{type:String,unique:true, required:true},
-      password:{type:String, required:true}
+      password:{type:String, required:true},
+      phoneNo:{type:Number,required:true},
+      address:{type:String,required:true}
 })
 const User=model("user",userSchema)
 export default User
