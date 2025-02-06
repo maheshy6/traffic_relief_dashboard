c68ac2fc01aab53a8adba7b2a4a0ac779004cd62
maheshy6 <maheshy675@gmail.com>
2025-02-06 20:05:43 +0530

    #db change

--- model/user.model.js
+++ model/user.model.js
@@ -3,8 +3,8 @@ const userSchema=new Schema({
       username:{type:String, required:true},
       email:{type:String,unique:true, required:true},
       password:{type:String, required:true},
-      phoneNo:{type:Number,required:true},
-      address:{type:String,required:true}
+      // phoneNo:{type:Number,required:true},
+      // address:{type:String,required:true}
 })
 const User=model("user",userSchema)
 export default User
